7eefea6efc87a416e765ae05ff54ed21e47b831f
Felk, Wilhelm <Wilhelm.Felk@adesso.de>
2022-12-20 16:25:00 +0100

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41f91e3dbc8a8c14"; }
+/******/ 		__webpack_require__.h = function() { return "9f8540dbb57a7a70"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
