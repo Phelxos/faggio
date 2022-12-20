0fcb311cd45a6ef9526ad6ae2d83d51f7c741fbf
Wilhelm Felk <69935260+Khrononaut@users.noreply.github.com>
2022-12-20 10:34:08 +0100

    refactor component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a3c640a5a2c1d43"; }
+/******/ 		__webpack_require__.h = function() { return "dfc34c74ae53bc57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/icons/Icon.tsx
+++ components/icons/Icon.tsx
@@ -5,15 +5,17 @@ import TIcon from "../../typings/types/TIcon";
 interface Props {
   icon: TIcon;
   className?: string;
+  onClick?: () => void;
 }
 
-export default function Icon({ className, icon }: Props) {
+export default function Icon({ className, icon, onClick }: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
       className={`${className}`}
+      onClick={onClick}
     >
       <IconPath icon={icon} />
     </svg>
