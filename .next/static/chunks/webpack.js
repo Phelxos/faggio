236e0949352f00091d61370e6d2a14a093c600a0
Wilhelm Felk <69935260+Khrononaut@users.noreply.github.com>
2022-12-20 16:32:32 +0100

    redirect /home requests to the index file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34d94f912c8659d7"; }
+/******/ 		__webpack_require__.h = function() { return "0a84aa4ad7340fde"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
