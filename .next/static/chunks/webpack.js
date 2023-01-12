5f9ac2efa701044fdf5a69b6d38050bdf6b18921
Wilhelm Felk <khrononaut@icloud.com>
2023-01-12 10:25:46 +0100

    save the globally displayed office in the store while keeping a contextual displayed office for the teams page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "254015799f2a866c"; }
+/******/ 		__webpack_require__.h = function() { return "b545d6ef2d1c3335"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
