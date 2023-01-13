91bcee3cb803ecad73e93ab9ff4ceffdefb6a64a
Buschbeck, Leonie <Leonie.Buschbeck@adesso.de>
2023-01-13 15:28:28 +0100

    Create MybOoking component

---
+++ components/bookings/bookingsMy/MyBooking.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function MyBooking() {
+  return (
+    <div className="flex w-full items-center justify-around rounded-t-lg bg-emerald-900 p-6 mt-10">
+      Helloooo
+    </div>
+  );
+}
