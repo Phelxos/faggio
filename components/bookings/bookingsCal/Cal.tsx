9ede1aec296575b92c7c0ea5629a390022228601
Buschbeck, Leonie <Leonie.Buschbeck@adesso.de>
2023-01-20 17:37:24 +0100

    Add new components to calendar page

--- components/bookings/bookingsCal/Cal.tsx
+++ components/bookings/bookingsCal/Cal.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import CalHeader from "./CalHeader";
 import CalBody from "./CalBody";
 import CalFooter from "./CalFooter";
+import MyBookingHeader from "../bookingsMy/MyBookingHeader";
+import MyBooking from "../bookingsMy/myBooking";
 
 export default function Calendar() {
   return (
-    <div className="flex h-[475px] w-full flex-col rounded-lg bg-emerald-600 sm:h-[600px] sm:w-[600px]">
-      <CalHeader />
-      <CalBody />
-      <CalFooter />
+    <div>
+      <div className="flex h-[475px] w-full flex-col rounded-lg bg-emerald-600 sm:h-[600px] sm:w-[600px]">
+        <CalHeader />
+        <CalBody />
+        <CalFooter />
+      </div>
+      <MyBookingHeader />
+      <MyBooking />
     </div>
+    
+    
   );
 }
