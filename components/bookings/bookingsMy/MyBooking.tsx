3f18e8bd9edd39bf4fc7fba3fbdf0df06fd8063f
Buschbeck, Leonie <Leonie.Buschbeck@adesso.de>
2023-01-25 15:06:30 +0100

    Update MyBooking component

--- components/bookings/bookingsMy/MyBooking.tsx
+++ components/bookings/bookingsMy/MyBooking.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-// import SearchAndSelect from "../../SearchAndSelect";
-// import { EOfficesEnglishToGerman } from "../../../typings/types/TOfficeCity";
 import StatusLocation from "../../header/status/StatusLocation";
 import Icon from "../../icons/Icon";
 
-
+interface Props {
+  bookedSlots: any[];
+}
 
 export default function MyBooking() {
+  const bookedSlots: any[] = [];
   return (
-    <div className="flex w-full justify-around rounded-lg bg-emerald-500 p-6 mt-10">
-        <div className="flex flex-col">
-            <p>Datum</p>
-            <StatusLocation color="slate" />
-        </div>
-        <button className="bg-red-600 hover:bg-red-400 text-white font-bold py-2 px-4 rounded-full">
-            <Icon icon="x-mark" className="h-5 w-5 mx-0"/>
-        </button>
-    </div>
-  );
+    // {bookedSlots.map((slot) => (
+      <div className="flex w-full justify-around rounded-lg bg-emerald-500 p-6 mt-10">
+          <div className="flex flex-col">
+              <p>datum</p>
+              <p>office</p>
+              <StatusLocation color="slate" />
+          </div>
+          <button className="bg-red-600 hover:bg-red-400 text-white font-bold py-2 px-4 rounded-full">
+              <Icon icon="x-mark" className="h-5 w-5 mx-0"/>
+          </button>
+      </div>
+    //   ))
+    // }
+  )
 }
