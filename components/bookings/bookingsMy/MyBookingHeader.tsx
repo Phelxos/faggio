e1cecca1d1a46c0a214be5e2f2b48f843e46c4f1
Buschbeck, Leonie <Leonie.Buschbeck@adesso.de>
2023-01-25 13:56:53 +0100

    Add dropdown to chose office

--- components/bookings/bookingsMy/MyBookingHeader.tsx
+++ components/bookings/bookingsMy/MyBookingHeader.tsx
@@ -1,22 +1,47 @@
 import React from "react";
-// import SearchAndSelect from "../../SearchAndSelect";
-// import { EOfficesEnglishToGerman } from "../../../typings/types/TOfficeCity";
-import StatusLocation from "../../header/status/StatusLocation";
+import SearchAndSelect from "../../SearchAndSelect";
+import useOffice from "../../../stores/SOffices";
 
 
 
 
 export default function MyBookingHeader() {
+  const allOfficeNames = useOffice((s) => s.allOfficeNames);
+  const displayedOffice = useOffice((s) => s.displayedOffice);
+  const setDisplayedOffice = useOffice((s) => s.setDisplayedOffice);
+
   return (
-    <div className="flex w-full items-center justify-around rounded-t-lg bg-emerald-900 p-6 mt-10">
-        <div className="font-mono font-thin text-slate-50">
-          Meine XX Buchungen in <StatusLocation color="slate" />
+    <div className="flex flex-col w-full items-center justify-around rounded-t-lg bg-emerald-900 p-6 mt-10">
+        <div className="font-mono font-thin text-slate-50 mb-5">
+          Meine XX (bookedSlots.length filtered nach Standort) Buchungen in 
+          </div>
+        <div className="w-1/2">
+          <SearchAndSelect
+            value={displayedOffice}
+            setValue={(event) => setDisplayedOffice(event)}
+            listOfValues={allOfficeNames} 
+          />
         </div>
-        {/* <SearchAndSelect
-          value={EOfficesEnglishToGerman}
-          setValue={(event) => console.log(event)}
-          listOfValues={EOfficesEnglishToGerman}
-        /> */}
+          
     </div>
   );
 }
+
+// bookedSlots = [
+//   {
+//     datum: "tt.mm.jjjj",
+//     office: "standort",
+//   },
+//   {
+//     datum: "tt.mm.jjjj",
+//     office: "standort",
+//   },
+//   {
+//     datum: "tt.mm.jjjj",
+//     office: "standort",
+//   },
+//   . 
+//   . 
+//   . 
+// ]
+// XX Buchungen: sowas wie bookedSlots.length einfügen
