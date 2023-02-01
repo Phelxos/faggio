29065dbe6211a0fc159342216286bc38b197d07e
Wilhelm Felk <khrononaut@icloud.com>
2023-02-01 13:52:15 +0100

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
 import "../styles/globals.scss";
-import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import ContextProvider from "../components/contexts/ContextProvider";
 import useSetupData from "../hooks/useSetupData";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useSetupData();
-  const [isHydrated, setIsHydrated] = useState(false);
-
-  // wait till NextJS rehydration completes
-  useEffect(() => {
-    setIsHydrated(true);
-  }, []);
+  const isHydrated = useSetupData();
 
   return (
     <>
