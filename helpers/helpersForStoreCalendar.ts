5222d3d7616d82d6a6469c809c6907225e1ba1cf
Wilhelm Felk <khrononaut@icloud.com>
2023-02-08 09:42:20 +0100

    return all working days of a year with t heir corresponding cal weeks

---
+++ helpers/helpersForStoreCalendar.ts
@@ -0,0 +1,81 @@
+import {
+  startOfMonth,
+  endOfMonth,
+  isWeekend,
+  getWeek,
+  nextMonday,
+  eachDayOfInterval,
+} from "date-fns";
+
+export function getDisplayedCalWeeksInSelectedMonth(
+  month?: number,
+  year?: number
+): number[] {
+  let today;
+  if (arguments.length) {
+    today = new Date(year!, month!);
+  } else {
+    today = new Date();
+  }
+  let firstDay = startOfMonth(today);
+  if (isWeekend(firstDay)) {
+    firstDay = nextMonday(firstDay);
+  }
+  const lastDay = endOfMonth(today);
+  const firstWeek = getWeek(firstDay, {
+    weekStartsOn: 1,
+    firstWeekContainsDate: 4,
+  });
+  const lastWeek = getWeek(lastDay, {
+    weekStartsOn: 1,
+    firstWeekContainsDate: 4,
+  });
+  return Array.from(
+    { length: lastWeek - firstWeek + 1 },
+    (_, i) => firstWeek + i
+  );
+}
+
+export function getLastDayOfCurrentYear(): Date {
+  const currentYear = new Date().getFullYear();
+  return new Date(currentYear, 11, 31);
+}
+
+export function getFirstCalWeekOfSelectedMonth(
+  month: number,
+  year: number
+): number {
+  const firstDayOfMonth = startOfMonth(new Date(year, month));
+  if (isWeekend(firstDayOfMonth)) {
+    return getWeek(nextMonday(firstDayOfMonth));
+  } else {
+    return getWeek(firstDayOfMonth);
+  }
+}
+
+export function getAllWorkingDaysOfYearWithTheirCorrespondingCalWeek(
+  year: number
+) {
+  const allWorkingDaysOfYear: Date[] = eachDayOfInterval({
+    start: new Date(year, 0, 1),
+    end: new Date(year, 11, 31),
+  });
+  const allWorkingDaysOfYearWithTheirCorrespondingCalWeek: (
+    | undefined
+    | {
+        date: Date;
+        calWeek: number;
+      }
+  )[] = allWorkingDaysOfYear
+    .map((date) => {
+      if (!isWeekend(date)) {
+        const calWeek = getWeek(date);
+        return {
+          date,
+          calWeek,
+        };
+      }
+    })
+    .filter((date) => date);
+  return allWorkingDaysOfYearWithTheirCorrespondingCalWeek;
+}
