55363cc358c79ddad2b7c1b430f323c46c0945f0
Wilhelm Felk <khrononaut@icloud.com>
2023-02-14 14:06:04 +0100

    change all relevant calendar store attributes to today's

--- components/bookings/bookingsCal/CalFooter.tsx
+++ components/bookings/bookingsCal/CalFooter.tsx
@@ -10,29 +10,37 @@ import useViewportDistance from "../../../hooks/useViewportDistance";
 export default function CalFooter() {
   const setSelectedMonth = useCalendar((s) => s.setSelectedMonth);
   const setSelectedYear = useCalendar((s) => s.setSelectedYear);
+  const setSelectedCalWeek = useCalendar((s) => s.setSelectedCalWeek);
   const currentMonth = useCalendar((s) => s.currentMonth);
   const currentYear = useCalendar((s) => s.currentYear);
+  const currentCalWeek = useCalendar((s) => s.currentCalWeek);
+  const today = useCalendar((s) => s.today);
   const allOfficeNames = useOffice((s) => s.allOfficeNames);
   const globallySelectedOfficeName = useOffice(
     (s) => s.globallySelectedOfficeName
   );
+  const setToday = useCalendar((s) => s.setToday);
   const c = useContext(CBookings);
   const inputContainerRef = useRef<HTMLDivElement | null>(null);
   const theme = useTheme();
   const { isCloserToTop } = useViewportDistance(inputContainerRef);
 
+  const setAllRelevantCalendarStoreVariablesToTodaysAttributes = () => {
+    setToday(today);
+    setSelectedMonth(currentMonth);
+    setSelectedYear(currentYear);
+    setSelectedCalWeek(currentCalWeek);
+  };
+
   useEffect(() => {
     c?.setLocallySelectedOfficeName(globallySelectedOfficeName);
-  }, []);
+  }, [globallySelectedOfficeName]);
 
   return (
     <div className="flex w-full items-center justify-center gap-6 rounded-b-lg bg-emerald-800 py-6 px-4">
       <button
         className="w-min rounded-lg border-2 border-emerald-400 bg-emerald-600 px-3 py-2 uppercase tracking-widest text-emerald-200 hover:bg-emerald-400 hover:text-emerald-600"
-        onClick={() => {
-          setSelectedMonth(currentMonth);
-          setSelectedYear(currentYear);
-        }}
+        onClick={setAllRelevantCalendarStoreVariablesToTodaysAttributes}
       >
         heute
       </button>
