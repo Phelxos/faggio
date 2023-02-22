b04c057fcee00519758d5a24f786e067b3de0aa6
Wilhelm Felk <khrononaut@icloud.com>
2023-02-22 18:07:34 +0100

    create header of greeting

--- components/home/Greeting.tsx
+++ components/home/Greeting.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useGreeting from "../../stores/SGreeting";
+import Icon from "../icons/Icon";
 
 export default function Greeting() {
   const greeting = useGreeting((s) => s.greeting);
@@ -7,15 +8,31 @@ export default function Greeting() {
   const user = "Wilhelm";
   return (
     <div className="w-full rounded bg-sky-600/50">
-      <div className="flex items-end gap-2 bg-sky-900">
-        <span className="text-3xl font-thin">{greeting}</span>
-        {", "}
-        <span>{user}</span>
+      <div className="flex items-end gap-2 rounded-t bg-sky-900 p-3">
+        <div>
+          <span className="text-3xl font-thin text-sky-300">{greeting}</span>
+          <span className="ml-1 font-light text-sky-300/50">,</span>
+        </div>
+        <div>
+          <span className="font-bold tracking-wide text-sky-400/50">
+            {user}
+          </span>
+          <span className=" ml-[2px] font-light text-sky-300/50">!</span>
+        </div>
       </div>
       <div>asldjfdölskjökl</div>
-      <div className="bg-sky-900">
-        {`„${greeting}“ bedeutet „Hallo“ in der Sprache `}
-        <span className="italic">{languageInGerman}</span>.
+      <div className="relative flex items-center justify-center gap-1 rounded-b bg-sky-900 p-2 text-xs text-sky-400/50">
+        <div>
+          <span className="italic">{greeting}</span> bedeutet „
+          <span>Hallo</span>“ in der Sprache{" "}
+        </div>
+        <a
+          className="mx-1 flex items-center gap-1 rounded bg-sky-800 p-[6px] text-sky-300/75"
+          href={`https://www.google.de/search?q=${languageInGerman}`}
+        >
+          <span>{languageInGerman}</span>
+          <Icon icon="arrowTopRightOnSquare" className="h-4 w-4 opacity-50" />
+        </a>
       </div>
     </div>
   );
