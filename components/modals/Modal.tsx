2df4d8b2f689746bfbaf9b2c5e0f2ae59a167182
Wilhelm Felk <khrononaut@icloud.com>
2023-03-21 10:14:29 +0100

    revise modal style

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -41,30 +41,25 @@ export default function Modal({
       onClose={toggleModal}
     >
       <Dialog.Panel className="max-h-3/4 min-h-1/4 max-w-3/4 min-w-1/4 flex h-[500px] w-[300px] flex-col items-center justify-between rounded-t rounded-b-xl border-t-8 border-t-sky-300/50 bg-sky-900/80 px-6 pt-4 pb-8 drop-shadow-2xl">
-        <div className="flex flex-col items-center gap-4">
-          <Dialog.Title className="text-2xl font-extralight tracking-widest text-sky-300">
-            {title}
-          </Dialog.Title>
-          <Dialog.Description className="flex items-center gap-2 rounded-lg bg-sky-600/50 px-2 py-1 text-sm font-bold tracking-wider text-black/75 drop-shadow-md">
-            <Icon
-              icon="informationCircle"
-              className="h-20 w-20 fill-black/25"
-            />
-            <p>{descriptionShort}</p>
-          </Dialog.Description>
-        </div>
-        <div className="flex grow flex-col items-center justify-center p-2 text-lg text-sky-200/75">
+        <Dialog.Title className="text-4xl font-thin tracking-widest text-sky-300">
+          {title}
+        </Dialog.Title>
+        <Dialog.Description className="flex items-center gap-2 rounded bg-sky-600/50 px-2 py-1 text-sm font-bold tracking-wider text-black/75 drop-shadow-md">
+          <Icon icon="informationCircle" className="h-20 w-20 fill-black/25" />
+          <p>{descriptionShort}</p>
+        </Dialog.Description>
+        <div className="flex flex-col items-center justify-center p-2 text-lg text-sky-200/75">
           <p>{descriptionLong}</p>
         </div>
         <div className="flex w-full items-stretch gap-4">
           <button
-            className="w-1/2 rounded bg-sky-500 p-4 font-mono text-sm font-bold uppercase tracking-wider text-black/75 drop-shadow-xl"
+            className="w-1/2 rounded-lg bg-sky-500 p-4 font-mono text-sm font-bold uppercase tracking-wider text-black/75 drop-shadow-xl"
             onClick={handleButtonAcceptClick}
           >
             {buttonAcceptLabel}
           </button>
           <button
-            className="w-1/2 rounded border-2 border-sky-200/75 p-4 font-mono text-sm uppercase tracking-wider text-white/75 drop-shadow-xl"
+            className="w-1/2 rounded-lg border-2 border-sky-200/75 p-4 font-mono text-sm uppercase tracking-wider text-white/75 drop-shadow-xl"
             onClick={handleButtonDeclineClick}
           >
             {buttonDeclineLabel}
