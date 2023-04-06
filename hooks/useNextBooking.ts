003f29eabc31eec310b135a4cab8a16cd5df844a
Wilhelm Felk <khrononaut@icloud.com>
2023-04-06 16:42:26 +0200

    Update next booking when date changes

--- hooks/useNextBooking.ts
+++ hooks/useNextBooking.ts
@@ -1,6 +1,9 @@
+import { useEffect, useRef } from "react";
 import useBookings from "../stores/SBookings";
+import useCalendar from "../stores/SCalendar";
 import { isAfter } from "date-fns";
 import { TOfficeCityEnglish } from "../typings/types/TOfficeCity";
+import IBooking from "../typings/interfaces/IBooking";
 
 export default function useNextBooking(id: number):
   | {
@@ -8,14 +11,27 @@ export default function useNextBooking(id: number):
       office: TOfficeCityEnglish;
     }
   | undefined {
-  const today = new Date();
+  const today = useCalendar((s) => s.today);
   const bookings = useBookings((s) => s.bookingsOther);
+  const nextBooking = useRef<IBooking | undefined>(undefined);
 
-  const nextBooking = bookings.find(({ date, coworkerId }) => {
+  nextBooking.current = bookings.find(({ date, coworkerId }) => {
     if (isAfter(new Date(date), today))
       return isAfter(new Date(date), today) && coworkerId === id;
   });
-  return nextBooking
-    ? { date: new Date(nextBooking!.date), office: nextBooking.office }
+
+  useEffect(() => {
+    nextBooking.current = bookings.find(({ date, coworkerId }) => {
+      if (isAfter(new Date(date), today))
+        return isAfter(new Date(date), today) && coworkerId === id;
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [today]);
+
+  return nextBooking.current
+    ? {
+        date: new Date(nextBooking.current!.date),
+        office: nextBooking.current!.office,
+      }
     : undefined;
 }
