d2e8b3f594080dac9d7638e019d23e476774cce9
Wilhelm Felk <khrononaut@icloud.com>
2023-05-16 14:32:22 +0200

    Style display message for wrong dimensions

--- hooks/usePhoneSize.tsx
+++ hooks/usePhoneSize.tsx
@@ -1,28 +1,4 @@
-import { is } from "date-fns/locale";
-import React, { useEffect, useState } from "react";
-
-function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
-  });
-
-  function handleResize() {
-    setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return windowSize;
-}
+import useWindowSize from "./useWindowSize";
 
 export default function usePhoneSize() {
   const size = useWindowSize();
