c40aaa7648aa45ce4afd71917bc7bacd32e262ed
Wilhelm Felk <khrononaut@icloud.com>
2023-06-27 17:21:25 +0200

    Create profile modal

---
+++ components/modals/ModalProfile/ModalProfile.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState, FC } from "react";
+import { Dialog } from "@headlessui/react";
+import IModal from "../../../typings/interfaces/IModal";
+import ModalProfileTile from "./ModalProfileTile";
+
+const ModalProfile: FC<IModal> = ({ isDisplayingModal, toggleModal }) => {
+  return (
+    <Dialog
+      className="fixed left-0 top-0 flex h-screen w-screen flex-col items-center justify-center bg-slate-900/50 backdrop-blur"
+      open={isDisplayingModal}
+      onClose={toggleModal}
+    >
+      <Dialog.Panel className="max-h-3/4 min-h-1/4 max-w-3/4 min-w-1/4 relative flex h-[500px] w-[300px] flex-col gap-8 rounded-b-lg rounded-t border-t-8 border-t-sky-300/50 bg-sky-900/75 p-4 drop-shadow-2xl">
+        <Dialog.Title className="text-center text-4xl font-thin text-sky-300/75">
+          Mein Profil
+        </Dialog.Title>
+        <div className="grid h-full w-full grow grid-cols-2 gap-4">
+          <ModalProfileTile name="Profil" nameBold="ansehen" />
+          <ModalProfileTile name="Profil" nameBold="bearbeiten" />
+          <ModalProfileTile name="Farbschema ändern" />
+          <ModalProfileTile name="weitere Einstellungen" />
+        </div>
+        <div
+          className="w-full grow rounded-b-lg border-2 border-red-700/50 bg-sky-950 p-4 text-center font-mono uppercase tracking-widest text-red-500/75"
+          onClick={toggleModal}
+        >
+          Abmelden
+        </div>
+      </Dialog.Panel>
+    </Dialog>
+  );
+};
+
+export default ModalProfile;
---
+++ components/modals/ModalProfile/ModalProfileTile.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import TIcon from "../../../typings/types/TIcon";
+import Icon from "../../icons/Icon";
+
+interface ModalProfileTileProps {
+  name: string;
+  nameBold?: string;
+  icon?: TIcon;
+}
+
+const ModalProfileTile: FC<ModalProfileTileProps> = ({
+  name,
+  nameBold,
+  icon = "informationCircle",
+}) => {
+  return (
+    <div className="relative h-full w-full overflow-hidden hyphens-auto rounded border-2 border-sky-900 bg-sky-950 p-4 shadow-lg">
+      <span className="font-mono text-sm text-sky-200/50">
+        {name} {nameBold && <strong>{nameBold}</strong>}
+      </span>
+      <Icon
+        icon={icon}
+        className="absolute -bottom-5 -right-5 h-20  w-20 fill-sky-700/25"
+      />
+    </div>
+  );
+};
+
+export default ModalProfileTile;
