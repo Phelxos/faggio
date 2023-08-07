39f864d0e69f7002764ffdbb8b29dcfa5da9e448
Wilhelm Felk <khrononaut@icloud.com>
2023-08-07 12:11:27 +0200

    Create layout and the page itself for login page

---
+++ app/(pages)/login/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return (
+    <div className="bg-leafs flex h-full w-full flex-col justify-end bg-slate-800">
+      <div className="h-3/5 rounded-t-3xl border-t-8 border-t-slate-950 bg-slate-900 p-4"></div>
+    </div>
+  );
+};
+
+export default Page;
