17b3f06f17d5a89d67471ddd7d9e9aa8e2e05e81
Wilhelm Felk <khrononaut@icloud.com>
2023-08-08 09:33:16 +0200

    Change color

--- app/(beforeLogIn)/login/components/SubmitButton.tsx
+++ app/(beforeLogIn)/login/components/SubmitButton.tsx
@@ -11,7 +11,7 @@ const SubmitButton: FC<Props> = ({ isValid }) => {
     <button
       type="submit"
       className={`focus:ring-none absolute bottom-0 left-0 right-0 h-1/6 w-screen bg-slate-400 text-2xl font-bold uppercase tracking-widest text-slate-700/50 outline-offset-2 drop-shadow-xl transition-all duration-200 focus:bg-yellow-500/50 focus:text-yellow-600 focus:outline-none ${
-        isValid && "bg-green-500/70 text-green-700"
+        isValid && "bg-green-500 text-green-700"
       }`}
     >
       anmelden
