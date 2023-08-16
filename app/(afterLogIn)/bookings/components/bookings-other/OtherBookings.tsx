5ce0a35e5c6fb73e4967173b98512d1101ebd605
Wilhelm Felk <khrononaut@icloud.com>
2023-08-16 11:33:07 +0200

    Style layout of bookings page

--- app/(afterLogIn)/bookings/components/bookings-other/OtherBookings.tsx
+++ app/(afterLogIn)/bookings/components/bookings-other/OtherBookings.tsx
@@ -3,7 +3,7 @@ import Main from "./main/Main";
 
 export default function BookingsOther() {
   return (
-    <div className="rounded-lg border-8 border-emerald-900/50">
+    <div className="col-span-full h-fit rounded-lg border-8 border-emerald-900/50 sm:col-start-3 sm:col-end-11 xl:col-start-4 xl:col-end-10 2xl:col-start-9 2xl:col-end-13">
       <Header />
       <Main />
     </div>
--- app/(afterLogIn)/bookings/components/calendar/Calendar.tsx
+++ app/(afterLogIn)/bookings/components/calendar/Calendar.tsx
@@ -7,13 +7,13 @@ import Footer from "./elements/Footer";
 import Header from "./elements/Header";
 
 const Calendar: FC = () => {
-  const c = useContext(BookingsContext);
+  const context = useContext(BookingsContext);
   return (
     <div
-      className={`flex h-[485px] w-full flex-col rounded-lg border-[6px] ${
-        c?.isBeingEdited ? "border-lime-300" : "border-emerald-900"
+      className={`col-span-full flex h-[485px] w-full flex-col rounded-lg border-[6px] sm:col-start-2 sm:col-end-12 lg:col-start-1 lg:col-end-7 lg:row-start-1 lg:row-end-2 xl:col-start-2 2xl:col-start-1 2xl:col-end-5 ${
+        context?.isBeingEdited ? "border-lime-300" : "border-emerald-900"
       } bg-slate-700`}
-      ref={c?.calRef}
+      ref={context?.calRef}
     >
       <Header />
       <Body />
