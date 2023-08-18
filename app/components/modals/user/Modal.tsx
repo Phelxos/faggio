b25697aede2d6256a45ae742beb32d15bd5d1a5c
Wilhelm Felk <khrononaut@icloud.com>
2023-08-18 16:25:43 +0200

    Remove error displaying unintentional error message

--- app/components/modals/user/Modal.tsx
+++ app/components/modals/user/Modal.tsx
@@ -33,43 +33,43 @@ const Modal: FC<IModal & { id: number }> = ({
 
   return isLoadingStoreData ? (
     <Spinner />
-  ) : coworker ? (
-    <Dialog
-      className="fixed left-1/2 top-1/2 flex h-screen w-screen -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-slate-900/50 backdrop-blur"
-      open={isDisplayingModal}
-      onClose={toggleModal}
-    >
-      <Dialog.Panel className="relative m-4 flex flex-col rounded-b-lg rounded-t border-t-8 border-t-pink-300/50 bg-pink-900/75 drop-shadow-2xl">
-        <div className="grid w-full grid-cols-4 grid-rows-[minmax(1fr,_fit-content)] place-items-center gap-y-4 overflow-auto px-6 pb-8 pt-4">
-          <Title forename={coworker?.forename} surname={coworker?.surname} />
-          <Image
-            src={coworker?.imgSrc || ""}
-            alt={`${coworker?.forename} ${coworker?.surname}`}
-            width={85}
-            height={85}
-            className="col-span-2 m-2 rounded-full border-2 border-pink-200/50 shadow-md"
-          />
-          <Description position={coworker?.position} />
-          <div className="col-span-4 flex h-full w-full items-center justify-between gap-2">
-            <Birthday
-              // Type assertion is valid here due to default props in the component
-              date={coworker?.birthdayDate as number}
-              month={coworker?.birthdayMonth as number}
-            />
-            <Office officeId={coworker?.officeId} />
-          </div>
-          <NextBooking id={coworker?.coworkerId} />
-          <Projects />
-        </div>
-        <UserCardControls
-          className="w-full rounded-b-lg"
-          phone={coworker?.phone}
-          email={coworker?.email}
-        />
-      </Dialog.Panel>
-    </Dialog>
   ) : (
-    <p>Fehler beim Laden</p>
+    coworker && (
+      <Dialog
+        className="fixed left-1/2 top-1/2 flex h-screen w-screen -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-slate-900/50 backdrop-blur"
+        open={isDisplayingModal}
+        onClose={toggleModal}
+      >
+        <Dialog.Panel className="relative m-4 flex flex-col rounded-b-lg rounded-t border-t-8 border-t-pink-300/50 bg-pink-900/75 drop-shadow-2xl">
+          <div className="grid w-full grid-cols-4 grid-rows-[minmax(1fr,_fit-content)] place-items-center gap-y-4 overflow-auto px-6 pb-8 pt-4">
+            <Title forename={coworker?.forename} surname={coworker?.surname} />
+            <Image
+              src={coworker?.imgSrc || ""}
+              alt={`${coworker?.forename} ${coworker?.surname}`}
+              width={85}
+              height={85}
+              className="col-span-2 m-2 rounded-full border-2 border-pink-200/50 shadow-md"
+            />
+            <Description position={coworker?.position} />
+            <div className="col-span-4 flex h-full w-full items-center justify-between gap-2">
+              <Birthday
+                // Type assertion is valid here due to default props in the component
+                date={coworker?.birthdayDate as number}
+                month={coworker?.birthdayMonth as number}
+              />
+              <Office officeId={coworker?.officeId} />
+            </div>
+            <NextBooking id={coworker?.coworkerId} />
+            <Projects />
+          </div>
+          <UserCardControls
+            className="w-full rounded-b-lg"
+            phone={coworker?.phone}
+            email={coworker?.email}
+          />
+        </Dialog.Panel>
+      </Dialog>
+    )
   );
 };
 
